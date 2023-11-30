e9b57cc4b6564ac4f2f3d49ea387c84ebea3cc78
Tenvy <riskypabji@gmail.com>
2023-11-30 20:04:04 +0800

    chore: removing unused variable

--- src/app/api/projects/[id]/route.ts
+++ src/app/api/projects/[id]/route.ts
@@ -2,43 +2,55 @@ import { projectType } from "@/types/projects";
 import prisma from "@/utils/db";
 import { NextResponse } from "next/server";
 
-export async function GET( context: { params: { id: string } } ) {
+export async function GET(context: { params: { id: string } }) {
     const id = context.params.id;
     try {
         const project = await prisma.projects.findUnique({
             where: {
-                id
-            }
-        })
-        if (!project) return NextResponse.json({msg: "project not found"})
-        return NextResponse.json(project)
+                id,
+            },
+        });
+        if (!project) return NextResponse.json({ msg: "project not found" });
+        return NextResponse.json(project);
     } catch (error) {
-        return NextResponse.json({msg: "failed to get project", error})
+        return NextResponse.json({ msg: "failed to get project", error });
     }
 }
 
-export async function PATCH( request:Request, context: { params: { id: string } } ) {
-    const { title, description, content, images, siteUrl, sourceUrl, techStack }:projectType = await request.json();
+export async function PATCH(
+    request: Request,
+    context: { params: { id: string } }
+) {
+    const {
+        title,
+        description,
+        content,
+        images,
+        siteUrl,
+        sourceUrl,
+        techStack,
+    }: projectType = await request.json();
     const id = context.params.id;
     try {
-        if(title) {
+        if (title) {
             const projectCheck = await prisma.projects.findUnique({
                 where: {
-                    title
-                }
-            })
-            if (projectCheck) return NextResponse.json({msg: "Project title is already exists!"})
+                    title,
+                },
+            });
+            if (projectCheck)
+                return NextResponse.json({ msg: "Project title is already exists!" });
         }
 
         await prisma.projectTech.deleteMany({
             where: {
-                projectId: id
-            }
-        })
+                projectId: id,
+            },
+        });
 
         const project = await prisma.projects.update({
             where: {
-                id
+                id,
             },
             data: {
                 title,
@@ -51,15 +63,43 @@ export async function PATCH( request:Request, context: { params: { id: string } 
                     create: techStack?.map((tech) => ({
                         tech: {
                             connect: {
-                                id: tech.id
-                            }
-                        }
-                    }))
-                }
-            }
-        })
-        return NextResponse.json({msg: "Project updated successfully", project})
+                                id: tech.id,
+                            },
+                        },
+                    })),
+                },
+            },
+        });
+        return NextResponse.json({ msg: "Project updated successfully", project });
     } catch (error) {
-        return NextResponse.json({msg: "Error updating Project", error})
+        return NextResponse.json({ msg: "Error updating Project", error });
+    }
+}
+
+export async function DELETE(
+    request: Request,
+    context: { params: { id: string } }
+) {
+    const id = context.params.id;
+    try {
+        const projectCheck = await prisma.projects.findUnique({
+            where: {
+                id,
+            },
+        });
+        if (!projectCheck) return NextResponse.json({ msg: "Project not found!" });
+        await prisma.projectTech.deleteMany({
+            where: {
+                projectId: id,
+            },
+        });
+        await prisma.projects.delete({
+            where: {
+                id,
+            },
+        });
+        return NextResponse.json({ msg: "Project deleted successfully" });
+    } catch (error) {
+        return NextResponse.json({ msg: "Error updating Project", error });
     }
 }
