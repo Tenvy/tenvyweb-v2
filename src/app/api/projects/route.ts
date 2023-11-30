3df389feba5c0de3370f7bd114423b98da56982b
Tenvy <riskypabji@gmail.com>
2023-11-30 17:27:32 +0800

    feat: GET and POST function for projects

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,12 +1,52 @@
 import prisma from "@/utils/db"
 import { projectType } from "@/types/projects"
+import { NextResponse } from "next/server"
+
+export async function GET(request: Request) {
+    try {
+        const projects = await prisma.projects.findMany({
+            include: {
+                techstack: true
+            }
+        })
+        return NextResponse.json(projects)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 export async function POST(request: Request) {
-    const { title, description, content, createDate, images, uuid, siteUrl, sourceUrl, techStack }:projectType = await request.json()
-
+    const { title, description, content, images, usersUuid, siteUrl, sourceUrl, techStack }:projectType = await request.json()
     try {
+        const project = await prisma.projects.findUnique({
+            where: {
+                title
+            }
+        })
+        if (project) return NextResponse.json({msg: "Project sudah ada"})
         
+        const created = await prisma.projects.create({
+            data: {
+                title,
+                description,
+                content,
+                images,
+                usersUuid,
+                siteUrl,
+                sourceUrl,
+                techstack: {
+                    create: techStack?.map(tech => ({
+                        tech: {
+                            connect: {
+                                id: tech.id
+                        }
+                }}))
+                }
+            }
+        })
+
+        return NextResponse.json({created})
     } catch (error) {
-        
+        console.log(error)
     }
 }
