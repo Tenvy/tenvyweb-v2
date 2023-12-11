550d23943f78e70d6b4c3facd050d28a9cadbb93
Tenvy <riskypabji@gmail.com>
2023-12-11 19:00:04 +0800

    chore: disabling cache

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -1,10 +1,10 @@
 const getProjects = async () => {
-    const response = await fetch(process.env.SITE_URL + `/api/projects`, {method: 'GET'})
+    const response = await fetch(process.env.SITE_URL + `/api/projects`, {method: 'GET', cache: 'no-store'})
     return response.json()
 }
 
 const getProjectById = async (id: string) => {
-    const response = await fetch(process.env.SITE_URL + `/api/projects/${id}` , {method: 'GET'})
+    const response = await fetch(process.env.SITE_URL + `/api/projects/${id}` , {method: 'GET', cache: 'no-store'})
     return response.json()
 }
 export { getProjects, getProjectById }
