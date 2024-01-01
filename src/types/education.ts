8ddf15348d31befdc59db8f903482f667965b085
tenvy <riskypabji@gmail.com>
2024-01-01 14:25:33 +0800

    chore: education data type

---
+++ src/types/education.ts
@@ -0,0 +1,9 @@
+export interface EducationProps {
+    school: string;
+    major: string;
+    images: string | null;
+    location: string;
+    degree: string;
+    startDate: string;
+    endDate: string | null;
+  }
