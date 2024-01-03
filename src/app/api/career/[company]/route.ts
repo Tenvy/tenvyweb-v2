14cba30201bee851a2b83323421776d8c580da48
tenvy <riskypabji@gmail.com>
2024-01-04 00:05:33 +0800

    feat: patch http request for career route

--- src/app/api/career/[company]/route.ts
+++ src/app/api/career/[company]/route.ts
@@ -1,6 +1,36 @@
 import prisma from "@/utils/db"
 import { NextResponse } from "next/server"
 
+export async function PATCH(request:Request, context: {params: { company: string}}) {
+    const { images, location, position, startDate, endDate } = await request.json()
+
+    const company = context.params.company
+    try {
+        const check = await prisma.career.findUnique({
+            where: {
+                company
+            }
+        })
+        if (!check) return NextResponse.json("Data Not Found")
+        const response = await prisma.career.update({
+            where: {
+                company
+            },
+            data: {
+                images,
+                location,
+                position,
+                startDate,
+                endDate
+            }
+        })
+        return NextResponse.json({response, msg: "Data Updated Successfully"})
+    } catch (error) {
+        return NextResponse.json(error)
+    }
+}
+
+
 export async function DELETE(request:Request, context: {params: { company: string}}) {
     const company = context.params.company
     try {
