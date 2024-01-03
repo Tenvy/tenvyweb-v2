eb50c7613d2a0f576635e559d6a5c28ccea07681
tenvy <riskypabji@gmail.com>
2024-01-04 00:05:15 +0800

    feat: patch http request for about route

--- src/app/api/about/[id]/route.ts
+++ src/app/api/about/[id]/route.ts
@@ -1,6 +1,31 @@
 import prisma from "@/utils/db"
 import { NextResponse } from "next/server"
 
+export async function PATCH(request:Request, context: {params: { id: string}}) {
+    const { story, isSelected } = await request.json()
+    const id = context.params.id
+    try {
+        const check = await prisma.about.findUnique({
+            where: {
+                id
+            }
+        })
+        if (!check) return NextResponse.json("Data Not Found")
+        const response = await prisma.about.update({
+            where: {
+                id
+            },
+            data: {
+                story,
+                isSelected
+            }
+        })
+        return NextResponse.json({response, msg: "Data Updated Successfully"})
+    } catch (error) {
+        return NextResponse.json(error)
+    }
+}
+
 export async function DELETE(request:Request, context: {params: { id: string}}) {
     const id = context.params.id
     try {
