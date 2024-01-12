9f3f3e1a133f2ee258e8078dc25c04bbf2649c97
tenvy <riskypabji@gmail.com>
2024-01-12 23:54:36 +0800

    feat: GET HTTP Request by company on career route

--- src/app/api/career/[company]/route.ts
+++ src/app/api/career/[company]/route.ts
@@ -1,6 +1,21 @@
 import prisma from "@/utils/db"
 import { NextResponse } from "next/server"
 
+export async function GET(request:Request, context: {params: { company: string}}) {
+    const company = context.params.company
+    try {
+        const response = await prisma.career.findUnique({
+            where: {
+                company
+            }
+        })
+        if (!response) return NextResponse.json("Data Not Found")
+        return NextResponse.json({response, msg: "Data Updated Successfully"})
+    } catch (error) {
+        return NextResponse.json(error)
+    }
+}
+
 export async function PATCH(request:Request, context: {params: { company: string}}) {
     const { images, location, position, startDate, endDate } = await request.json()
 
