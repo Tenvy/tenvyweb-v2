f5a402cd8414b2e6d3246f138ce7a40af923dc2d
tenvy <riskypabji@gmail.com>
2024-02-24 21:43:24 +0800

    feat: add conditional for story data

--- src/app/(main)/about/components/story.tsx
+++ src/app/(main)/about/components/story.tsx
@@ -1,12 +1,17 @@
+import EmptyState from '@/components/elements/EmptyState';
 import { getStory } from '@/services/story'
 
 const Story = async () => {
   const data = await getStory()
 
+  if(!data) return (
+    <EmptyState message='No Story Here'/>
+  )
+
   return (
     <section
       className='space-y-4 leading-[1.8] md:leading-loose text-neutral-300'
-      dangerouslySetInnerHTML={{ __html: data.story }}
+      dangerouslySetInnerHTML={{ __html: data?.story }}
     />
   );
 };
